0666bc885ff49f13c72cbcad002eda28c94aad89
abdullahanzar <31632960+abdullahanzar@users.noreply.github.com>
2023-11-26 13:39:04 +0530

    Main Functionality Complete, Styles Complete, Responsiveness Complete

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import LinkChecker from "@/components/LinkChecker";
 export default function Home() {
   return (
     <main>
-      <h1>ENTER YOUR URL BELOW</h1>
       <LinkChecker />
     </main>
   );
--- src/components/LinkChecker.js
+++ src/components/LinkChecker.js
@@ -2,45 +2,82 @@
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import styles from "./linkChecker.module.css";
 
 export default function LinkChecker() {
   const [url, setUrl] = useState("");
   const [result, setResult] = useState(null);
-
-  useEffect(() => {
-    console.log(result);
-  }, [result]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const checkLinks = async () => {
+    if (url == "") return setError("Please enter a link in the textbox!");
+    else {
+      setError("");
+      setResult(null);
+    }
     try {
+      setLoading(true);
       const response = await axios.get(
         `/api/checkLinks?url=${encodeURIComponent(url)}`
       );
       console.log(response);
       setResult(response.data);
+      setLoading(false);
     } catch (e) {
-      console.error("Error checking links:", e);
+      //console.error("Error checking links:", e);
+      setError("Main link is not working!");
+      setLoading(false);
     }
   };
   return (
-    <div>
-      <div className="urlTextBox">
-        <label htmlFor="url">
-          Enter URL:
-          <input
-            name="url"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-          />
-        </label>
+    <div className={styles.mainbody}>
+      <div className={styles.urlTextBox}>
+        <h1> &#x1F50D; Broken Link Checker </h1>
+        <input
+          name="url"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          placeholder="Enter your link here!"
+        />
         <button onClick={() => checkLinks()}>Check Links</button>
+        {loading && <span class={styles.loader}></span>}
+        {error && <span class={styles.error}>{error}</span>}
       </div>
-      {result && (
-        <div className="results">
-          {result.map((obj, key) => (
-            <li key={key}>{obj.link} {obj.status}</li>
-          ))}
+      {
+        <div className={styles.results}>
+          {result !== null && result !== undefined && result.length !== 0 && (
+            <p>
+              Main link is <span className={styles.ok}>working</span>, sublinks
+              on the page are:
+            </p>
+          )}
+          {result !== null && result !== undefined && result.length === 0 && (
+            <p>
+              Main link is <span className={styles.ok}>working</span>, there are
+              &nbsp;
+              <span className={styles.broken}>
+                no sublinks detected on the page.
+              </span>
+            </p>
+          )}
+          <ul>
+            {result &&
+              result.map((obj, key) => (
+                <li key={key}>
+                  <a href={obj.link}>{obj.link}</a>
+                  <p>
+                    Status:{" "}
+                    <span
+                      className={obj.status == "ok" ? styles.ok : styles.broken}
+                    >
+                      {obj.status}
+                    </span>
+                  </p>
+                </li>
+              ))}
+          </ul>
         </div>
-      )}
+      }
     </div>
   );
 }
